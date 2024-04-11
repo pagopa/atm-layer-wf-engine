905ad195d17aa4d27feb80cb6ed8636be8f722e6
GiacomoB <86237971+GiacomoBrancazi@users.noreply.github.com>
2024-04-11 11:52:27 +0200

    Feature/camunda rest controller (#34)

--- src/main/java/it/pagopa/wf/engine/controller/CamundaController.java
+++ src/main/java/it/pagopa/wf/engine/controller/CamundaController.java
@@ -5,20 +5,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
 
 @RestController
 @RequestMapping(value = "/camunda")
 public class CamundaController {
 
-
     @PostMapping(value = "/verify/bpmn", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
     @ResponseStatus(HttpStatus.OK)
-    public Boolean verifyBpmn(@RequestParam("file") MultipartFile file) {
-        try {
-            Bpmn.readModelFromStream(file.getInputStream());
-        } catch (Exception e) {
-            return Boolean.FALSE;
-        }
+    public Boolean verifyBpmn(@RequestParam("file") MultipartFile file) throws IOException {
+
+        Bpmn.readModelFromStream(file.getInputStream());
         return Boolean.TRUE;
     }
 }
