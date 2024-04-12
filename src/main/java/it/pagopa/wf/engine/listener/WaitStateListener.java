ac7cdf2a112bcffed498a9cbf915c86a7f209742
Giacomo Brancazi <giacomo.brancazi@emeal.nttdata.com>
2024-04-12 15:12:19 +0200

    add redis

--- src/main/java/it/pagopa/wf/engine/listener/WaitStateListener.java
+++ src/main/java/it/pagopa/wf/engine/listener/WaitStateListener.java
@@ -1,38 +1,33 @@
 package it.pagopa.wf.engine.listener;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import it.pagopa.wf.engine.model.Task;
 import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.ExecutionListener;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import redis.clients.jedis.Jedis;
+
+import java.util.Map;
 
 @Slf4j
 public class WaitStateListener implements ExecutionListener {
 
+    @Value("spring.redis.host")
+    private String redisHost;
 
-    private final String notificationCallbackBasePath;
-
-    public WaitStateListener(String notificationCallbackBasePath) {
-        this.notificationCallbackBasePath = notificationCallbackBasePath;
-    }
-
-    private final static String CALLBACK_CONTEXT_PATH = "/workflow/listener/notify/{requestId}";
+    @Value("spring.redis.port")
+    private int redisPort;
 
     @Override
     public void notify(DelegateExecution execution) {
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Content-Type", "application/json");
 
-        String apiUrl = notificationCallbackBasePath + CALLBACK_CONTEXT_PATH;
         String requestId = (String) execution.getVariable("requestId");
         String processInstanceId = execution.getProcessInstanceId();
         String currentActivityName = execution.getCurrentActivityName();
         String currentActivityId = execution.getCurrentActivityId();
         String processDefinitionId = execution.getProcessDefinitionId();
+
         Notification notification = Notification.builder()
                 .processInstanceId(processInstanceId)
                 .processDefinitionId(processDefinitionId)
@@ -40,21 +35,23 @@ public class WaitStateListener implements ExecutionListener {
                 .requestId(requestId)
                 .currentActivityId(currentActivityId)
                 .requestId(requestId).build();
-        HttpEntity<Notification> requestEntity = new HttpEntity<>(notification, headers);
-        String[] params = {requestId};
 
-        ResponseEntity<Void> response = restTemplate.exchange(
-                apiUrl,
-                HttpMethod.POST,
-                requestEntity,
-                Void.class,
-                (Object[]) params);
+        Task task = new Task();
+        task.setId(requestId);
+        task.setVariables(Map.of("key1", "value1", "key2", "value2"));
+        task.setForm("formKey");
+        task.setPriority(1);
 
-        // Gestione della risposta
-        if (response.getStatusCode().is2xxSuccessful()) {
-            log.info("Wait state notified for process with requestId: {}", requestId);
-        } else {
-            log.error("Failed to notify wait state for process with requestId: {} with status code: {}", requestId, response.getStatusCode());
+        try (Jedis jedis = new Jedis(redisHost, redisPort)) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            String taskJson = objectMapper.writeValueAsString(task);
+
+            jedis.publish(requestId, taskJson);
+            log.info("Messaggio pubblicato con successo sul topic.");
+        } catch (Exception e) {
+            log.error("Failed to notify wait state for process with requestId: {}", requestId);
+            e.printStackTrace();
         }
+
     }
 }
--- src/main/java/it/pagopa/wf/engine/parselistener/CustomUserTaskStartParseListener.java
+++ src/main/java/it/pagopa/wf/engine/parselistener/CustomUserTaskStartParseListener.java
@@ -8,21 +8,15 @@ import org.camunda.bpm.engine.impl.util.xml.Element;
 
 public class CustomUserTaskStartParseListener extends ProcessApplicationEventParseListener {
 
-    private final String notificationWaitStatusCallbackBasePath;
-
-    public CustomUserTaskStartParseListener(String notificationWaitStatusCallbackBasePath) {
-        this.notificationWaitStatusCallbackBasePath = notificationWaitStatusCallbackBasePath;
-    }
-
     @Override
     public void parseUserTask(Element userTaskElement, ScopeImpl scope, ActivityImpl activity) {
         super.parseUserTask(userTaskElement, scope, activity);
-        activity.addListener("start", new WaitStateListener(notificationWaitStatusCallbackBasePath));
+        activity.addListener("start", new WaitStateListener());
     }
 
     @Override
     public void parseEndEvent(Element endEventElement, ScopeImpl scope, ActivityImpl activity) {
         super.parseEndEvent(endEventElement, scope, activity);
-        activity.addListener("start", new WaitStateListener(notificationWaitStatusCallbackBasePath));
+        activity.addListener("start", new WaitStateListener());
     }
 }
