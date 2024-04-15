953eacce7bebdb7b87b5447372b8adc26a4161ea
GiacomoB <86237971+GiacomoBrancazi@users.noreply.github.com>
2024-04-15 18:43:10 +0200

    Poc redis pub sub (#48)
    Co-authored-by: Cocuzzoli, Andrea <andrea.cocuzzoli@nttdata.com>

--- src/main/java/it/pagopa/wf/engine/config/RedisProperty.java
+++ src/main/java/it/pagopa/wf/engine/config/RedisProperty.java
@@ -1,14 +1,10 @@
 package it.pagopa.wf.engine.config;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Component
 public class RedisProperty {
 
