9a911576396c62e3c64979319906e64b047fcbff
GiacomoB <86237971+GiacomoBrancazi@users.noreply.github.com>
2024-04-16 10:15:05 +0200

    Poc redis pub sub (#51)
    Co-authored-by: Cocuzzoli, Andrea <andrea.cocuzzoli@nttdata.com>

--- src/main/java/it/pagopa/wf/engine/listener/WaitStateListener.java
+++ src/main/java/it/pagopa/wf/engine/listener/WaitStateListener.java
@@ -3,25 +3,28 @@ package it.pagopa.wf.engine.listener;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import it.pagopa.wf.engine.config.RedisProperty;
 import it.pagopa.wf.engine.model.Task;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.ExecutionListener;
 import org.camunda.bpm.engine.impl.task.TaskDefinition;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import redis.clients.jedis.Jedis;
 
 @Slf4j
-@Component
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class WaitStateListener implements ExecutionListener {
 
-    @Autowired
-    RedisProperty redisProperty;
+    private RedisProperty redisProperty;
 
     private TaskDefinition taskDefinition;
 
+    public WaitStateListener(RedisProperty redisProperty){
+        this.redisProperty = redisProperty;
+    }
 
     @Override
     public void notify(DelegateExecution execution) {
@@ -35,23 +38,23 @@ public class WaitStateListener implements ExecutionListener {
         log.info(" BusinessKey = " + execution.getBusinessKey());
         log.info(" processInstanceId " + processInstanceId + " currentActivityName " + currentActivityName + " currentActivityId " + currentActivityId + " processDefinitionId " + processDefinitionId);
 
-        log.info("TaskDefinition = " + taskDefinition.toString() +
-                "\n FormKey = " + (taskDefinition.getFormKey() == null ? "is null" : taskDefinition.getFormKey().getExpressionText()) +
-                "\n Priority = " + (taskDefinition.getPriorityExpression() == null ? "is null" : taskDefinition.getPriorityExpression().getExpressionText()));
-
         Task task = new Task();
         task.setId(execution.getCurrentActivityId());
         task.setVariables(execution.getVariables());
-        task.setForm(taskDefinition.getFormKey() == null ? null : taskDefinition.getFormKey().getExpressionText());
-        int priority = 0;
-        try {
-            priority = taskDefinition.getPriorityExpression() == null ? 0 : Integer.parseInt(taskDefinition.getPriorityExpression().getExpressionText());
-        } catch (Exception e) {
-            log.error("priority not a number");
-        }
-        task.setPriority(priority);
 
-        try (Jedis jedis = new Jedis("pagopa-dev-atm-layer-redis.vab4cc.ng.0001.eus1.cache.amazonaws.com", 6379)) {
+        if(taskDefinition != null) {
+            log.info(" FormKey = " + (taskDefinition.getFormKey() == null ? " is null" : taskDefinition.getFormKey().getExpressionText()) +
+                    "\n Priority = " + (taskDefinition.getPriorityExpression() == null ? " is null" : taskDefinition.getPriorityExpression().getExpressionText()));
+            task.setForm(taskDefinition.getFormKey() == null ? null : taskDefinition.getFormKey().getExpressionText());
+            int priority = 0;
+            try {
+                priority = taskDefinition.getPriorityExpression() == null ? 0 : Integer.parseInt(taskDefinition.getPriorityExpression().getExpressionText());
+            } catch (Exception e) {
+                log.error("priority not a number");
+            }
+            task.setPriority(priority);
+        }
+        try (Jedis jedis = new Jedis(redisProperty.getRedisHost(), redisProperty.getRedisPort())) {
             ObjectMapper objectMapper = new ObjectMapper();
             String taskJson = objectMapper.writeValueAsString(task);
 
--- src/main/java/it/pagopa/wf/engine/model/Task.java
+++ src/main/java/it/pagopa/wf/engine/model/Task.java
@@ -1,14 +1,13 @@
 package it.pagopa.wf.engine.model;
 
-import java.util.Map;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Map;
 
 @Data
 @Builder
