c486770d7a91841569bf31b9efd675b1eddbaa89
Giacomo Brancazi <giacomo.brancazi@emeal.nttdata.com>
2024-04-16 09:54:10 +0200

    inject redisProperty

--- src/main/java/it/pagopa/wf/engine/config/CamundaConfiguration.java
+++ src/main/java/it/pagopa/wf/engine/config/CamundaConfiguration.java
@@ -3,7 +3,6 @@ package it.pagopa.wf.engine.config;
 import it.pagopa.wf.engine.parselistener.CustomUserTaskStartParseListener;
 import org.camunda.bpm.engine.impl.cfg.ProcessEngineConfigurationImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,9 +10,11 @@ import org.springframework.context.annotation.Configuration;
 @ConditionalOnProperty(prefix = "engine-notification.wait-status", name = "enabled", havingValue = "true", matchIfMissing = true)
 public class CamundaConfiguration {
     
+    @Autowired
+    CustomUserTaskStartParseListener customUserTaskStartParseListener;
 
     @Autowired
     public void configureProcessEngineConfiguration(ProcessEngineConfigurationImpl config) {
-        config.getCustomPostBPMNParseListeners().add(new CustomUserTaskStartParseListener());
+        config.getCustomPostBPMNParseListeners().add(customUserTaskStartParseListener);
     }
 }
--- src/main/java/it/pagopa/wf/engine/listener/WaitStateListener.java
+++ src/main/java/it/pagopa/wf/engine/listener/WaitStateListener.java
@@ -3,25 +3,24 @@ package it.pagopa.wf.engine.listener;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import it.pagopa.wf.engine.config.RedisProperty;
 import it.pagopa.wf.engine.model.Task;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.ExecutionListener;
 import org.camunda.bpm.engine.impl.task.TaskDefinition;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import redis.clients.jedis.Jedis;
 
 @Slf4j
-@Component
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class WaitStateListener implements ExecutionListener {
 
-    @Autowired
-    RedisProperty redisProperty;
+    private RedisProperty redisProperty;
 
     private TaskDefinition taskDefinition;
-
 
     @Override
     public void notify(DelegateExecution execution) {
