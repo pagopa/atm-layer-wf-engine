2942458f7cfa2d478fb415e88d84f8e9d75d113b
Giacomo Brancazi <giacomo.brancazi@emeal.nttdata.com>
2024-04-16 09:13:24 +0200

    set formkey and priority

--- src/main/java/it/pagopa/wf/engine/listener/WaitStateListener.java
+++ src/main/java/it/pagopa/wf/engine/listener/WaitStateListener.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.ExecutionListener;
+import org.camunda.bpm.engine.impl.task.TaskDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import redis.clients.jedis.Jedis;
@@ -19,6 +20,9 @@ public class WaitStateListener implements ExecutionListener {
     @Autowired
     RedisProperty redisProperty;
 
+    private TaskDefinition taskDefinition;
+
+
     @Override
     public void notify(DelegateExecution execution) {
 
@@ -29,13 +33,23 @@ public class WaitStateListener implements ExecutionListener {
 
         log.info(" execution = " + execution);
         log.info(" BusinessKey = " + execution.getBusinessKey());
-        log.info(" processInstanceId " + processInstanceId +" currentActivityName " +currentActivityName +" currentActivityId " +currentActivityId +" processDefinitionId "+ processDefinitionId);
+        log.info(" processInstanceId " + processInstanceId + " currentActivityName " + currentActivityName + " currentActivityId " + currentActivityId + " processDefinitionId " + processDefinitionId);
+
+        log.info("TaskDefinition = " + taskDefinition.toString() +
+                "\n FormKey = " + (taskDefinition.getFormKey() == null ? "is null" : taskDefinition.getFormKey().getExpressionText()) +
+                "\n Priority = " + (taskDefinition.getPriorityExpression() == null ? "is null" : taskDefinition.getPriorityExpression().getExpressionText()));
+
         Task task = new Task();
         task.setId(execution.getCurrentActivityId());
-        //todo
         task.setVariables(execution.getVariables());
-        task.setForm("formKey");
-        task.setPriority(1);
+        task.setForm(taskDefinition.getFormKey() == null ? null : taskDefinition.getFormKey().getExpressionText());
+        int priority = 0;
+        try {
+            priority = taskDefinition.getPriorityExpression() == null ? 0 : Integer.parseInt(taskDefinition.getPriorityExpression().getExpressionText());
+        } catch (Exception e) {
+            log.error("priority not a number");
+        }
+        task.setPriority(priority);
 
         try (Jedis jedis = new Jedis("pagopa-dev-atm-layer-redis.vab4cc.ng.0001.eus1.cache.amazonaws.com", 6379)) {
             ObjectMapper objectMapper = new ObjectMapper();
