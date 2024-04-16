69ab58a53ff9d98002a69e3ca6b2c0d612d1792b
Giacomo Brancazi <giacomo.brancazi@emeal.nttdata.com>
2024-04-16 10:14:24 +0200

    null safe for task definition

--- src/main/java/it/pagopa/wf/engine/listener/WaitStateListener.java
+++ src/main/java/it/pagopa/wf/engine/listener/WaitStateListener.java
@@ -38,22 +38,22 @@ public class WaitStateListener implements ExecutionListener {
         log.info(" BusinessKey = " + execution.getBusinessKey());
         log.info(" processInstanceId " + processInstanceId + " currentActivityName " + currentActivityName + " currentActivityId " + currentActivityId + " processDefinitionId " + processDefinitionId);
 
-        log.info("TaskDefinition = " + taskDefinition.toString() +
-                "\n FormKey = " + (taskDefinition.getFormKey() == null ? "is null" : taskDefinition.getFormKey().getExpressionText()) +
-                "\n Priority = " + (taskDefinition.getPriorityExpression() == null ? "is null" : taskDefinition.getPriorityExpression().getExpressionText()));
-
         Task task = new Task();
         task.setId(execution.getCurrentActivityId());
         task.setVariables(execution.getVariables());
-        task.setForm(taskDefinition.getFormKey() == null ? null : taskDefinition.getFormKey().getExpressionText());
-        int priority = 0;
-        try {
-            priority = taskDefinition.getPriorityExpression() == null ? 0 : Integer.parseInt(taskDefinition.getPriorityExpression().getExpressionText());
-        } catch (Exception e) {
-            log.error("priority not a number");
-        }
-        task.setPriority(priority);
 
+        if(taskDefinition != null) {
+            log.info(" FormKey = " + (taskDefinition.getFormKey() == null ? " is null" : taskDefinition.getFormKey().getExpressionText()) +
+                    "\n Priority = " + (taskDefinition.getPriorityExpression() == null ? " is null" : taskDefinition.getPriorityExpression().getExpressionText()));
+            task.setForm(taskDefinition.getFormKey() == null ? null : taskDefinition.getFormKey().getExpressionText());
+            int priority = 0;
+            try {
+                priority = taskDefinition.getPriorityExpression() == null ? 0 : Integer.parseInt(taskDefinition.getPriorityExpression().getExpressionText());
+            } catch (Exception e) {
+                log.error("priority not a number");
+            }
+            task.setPriority(priority);
+        }
         try (Jedis jedis = new Jedis(redisProperty.getRedisHost(), redisProperty.getRedisPort())) {
             ObjectMapper objectMapper = new ObjectMapper();
             String taskJson = objectMapper.writeValueAsString(task);
