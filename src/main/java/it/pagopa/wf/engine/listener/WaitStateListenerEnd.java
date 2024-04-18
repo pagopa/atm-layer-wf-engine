d4ebff3373f01acc3bb37d89d4999587e9866023
adrrossi <adriano.rossi@nttdata.com>
2024-04-18 11:24:12 +0200

    refactoring redisClient

--- src/main/java/it/pagopa/wf/engine/listener/WaitStateListenerEnd.java
+++ src/main/java/it/pagopa/wf/engine/listener/WaitStateListenerEnd.java
@@ -1,6 +1,7 @@
 package it.pagopa.wf.engine.listener;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import it.pagopa.wf.engine.client.RedisClient;
 import it.pagopa.wf.engine.config.RedisProperty;
 import it.pagopa.wf.engine.model.Task;
 import lombok.AllArgsConstructor;
@@ -17,22 +18,14 @@ import redis.clients.jedis.Jedis;
 @NoArgsConstructor
 public class WaitStateListenerEnd implements ExecutionListener {
 
-    private RedisProperty redisProperty;
+    private RedisClient redisClient;
 
     @Override
     public void notify(DelegateExecution execution) {
 
         Task task = new Task();
         if(execution.getParentId() == null || execution.getParentId().isBlank()) {
-            try (Jedis jedis = new Jedis(redisProperty.getRedisHost(), redisProperty.getRedisPort())) {
-                ObjectMapper objectMapper = new ObjectMapper();
-                String taskJson = objectMapper.writeValueAsString(task);
-                jedis.publish(execution.getBusinessKey(), taskJson);
-                log.info("Messaggio pubblicato con successo sul topic.");
-            } catch (Exception e) {
-                log.error("Failed to notify wait state for process");
-                e.printStackTrace();
-            }
+            redisClient.publish(execution.getBusinessKey(), task);
         }
     }
 }
