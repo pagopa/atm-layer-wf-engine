0316d45d10847ce683f84497e3db542bc6fda8ae
LuciaM1 <144435187+LuciaM1@users.noreply.github.com>
2024-05-06 10:49:40 +0200

    validate executable (#70)

--- src/main/java/it/pagopa/wf/engine/service/CamundaService.java
+++ src/main/java/it/pagopa/wf/engine/service/CamundaService.java
@@ -1,8 +1,7 @@
 package it.pagopa.wf.engine.service;
 
 import it.pagopa.wf.engine.model.VerifyResponse;
-import org.camunda.bpm.engine.ParseException;
-import org.camunda.bpm.engine.impl.bpmn.parser.BpmnParse;
+import it.pagopa.wf.engine.validator.BpmnCustomParse;
 import org.camunda.bpm.engine.impl.bpmn.parser.BpmnParser;
 import org.camunda.bpm.engine.impl.cfg.BpmnParseFactory;
 import org.camunda.bpm.engine.impl.cfg.DefaultBpmnParseFactory;
@@ -11,7 +10,6 @@ import org.camunda.bpm.engine.impl.context.Context;
 import org.camunda.bpm.engine.impl.el.ExpressionManager;
 import org.camunda.bpm.engine.impl.el.JuelExpressionManager;
 import org.camunda.bpm.engine.impl.persistence.entity.DeploymentEntity;
-import org.camunda.bpm.engine.impl.util.xml.Element;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -21,35 +19,28 @@ import java.io.InputStream;
 
 @Service
 public class CamundaService {
-
     @Autowired
     ProcessEngineConfigurationImpl processEngineConfiguration;
 
     public VerifyResponse validateFile(MultipartFile file) throws IOException {
         VerifyResponse response = new VerifyResponse();
-
         try (InputStream inputStream = file.getInputStream()) {
             final ExpressionManager testExpressionManager = new JuelExpressionManager();
-
             Context.setProcessEngineConfiguration(processEngineConfiguration);
-
             BpmnParseFactory bpmnParseFactory = new DefaultBpmnParseFactory();
             BpmnParser bpmnParser = new BpmnParser(testExpressionManager, bpmnParseFactory);
-            BpmnParse bpmnParse = bpmnParser.createParse()
+            BpmnCustomParse bpmnParse = (BpmnCustomParse) bpmnParser.createParse()
                     .sourceInputStream(inputStream)
                     .deployment(new DeploymentEntity())
                     .name(file.getName());
             bpmnParse.execute();
-
             response.setIsVerified(Boolean.TRUE);
-            response.setMessage("Corretc Bpmn");
+            response.setMessage("Correct Bpmn");
             return response;
-        }
-        catch (final Exception exception) {
+        } catch (Exception exception) {
             response.setIsVerified(Boolean.FALSE);
             response.setMessage(exception.getCause() == null ? exception.getMessage() : exception.getCause().getMessage());
             return response;
         }
     }
-
 }
