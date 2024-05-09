4d9136735becfb7964acc555f54d6aa6c3eeb30e
Giacomo Brancazi <giacomo.brancazi@emeal.nttdata.com>
2024-05-09 09:14:15 +0200

    revert all redis

--- src/main/java/it/pagopa/wf/engine/controller/CamundaController.java
+++ src/main/java/it/pagopa/wf/engine/controller/CamundaController.java
@@ -5,11 +5,7 @@ import it.pagopa.wf.engine.service.CamundaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
--- src/main/java/it/pagopa/wf/engine/service/CamundaService.java
+++ src/main/java/it/pagopa/wf/engine/service/CamundaService.java
@@ -17,7 +17,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.text.ParseException;
 
 import static org.camunda.bpm.engine.impl.bpmn.parser.BpmnParse.CAMUNDA_BPMN_EXTENSIONS_NS;
 
