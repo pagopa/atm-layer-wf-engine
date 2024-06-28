7ffd7c1b573c453d0ebfcf9939acd92452f7e28e
adrrossi <adriano.rossi@nttdata.com>
2024-06-28 10:56:31 +0200

    change callRest

--- src/main/java/it/pagopa/wf/engine/delegate/ExternalTaskDelegator.java
+++ src/main/java/it/pagopa/wf/engine/delegate/ExternalTaskDelegator.java
@@ -1,14 +1,14 @@
 package it.pagopa.wf.engine.delegate;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import it.pagopa.wf.engine.service.CallRestService;
-import it.pagopa.wf.engine.util.HttpRequestUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service("externalTaskDelegator")
 @Slf4j
@@ -20,9 +20,9 @@ public class ExternalTaskDelegator implements JavaDelegate {
 
 
     @Override
-    public void execute(DelegateExecution delegateExecution) {
-        HttpHeaders httpHeaders = HttpRequestUtils.createHttpHeaders(delegateExecution.getVariables());
-        log.info("--- Call Adapter with headers {}: and businessKey: {} ",httpHeaders, delegateExecution.getBusinessKey());
-        callRestService.callAdapter(httpHeaders);
+    public void execute(DelegateExecution delegateExecution) throws JsonProcessingException {
+        Map<String, Object> variables = delegateExecution.getVariables();
+        log.info("--- Call Adapter with variables {}: and businessKey: {} ",variables, delegateExecution.getBusinessKey());
+        callRestService.callAdapter(variables);
     }
 }
