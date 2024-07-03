4e1472ce568983af00880fdb259b3e9529990219
LuciaM1 <lucia.mazzocchi@nttdata.com>
2024-07-03 10:53:51 +0200

    fixing delegator getVariables

--- src/main/java/it/pagopa/wf/engine/delegate/ExternalTaskDelegator.java
+++ src/main/java/it/pagopa/wf/engine/delegate/ExternalTaskDelegator.java
@@ -21,7 +21,7 @@ public class ExternalTaskDelegator implements JavaDelegate {
 
     @Override
     public void execute(DelegateExecution delegateExecution) throws JsonProcessingException {
-        Map<String, Object> variables = delegateExecution.getVariablesLocal();
+        Map<String, Object> variables = delegateExecution.getVariables();
         variables.put("processInstanceId",delegateExecution.getProcessInstanceId());
         log.info("--- Call Adapter with variables {}: and businessKey: {} ",variables, delegateExecution.getBusinessKey());
 //        delegateExecution.setVariable("response", callRestService.callAdapter(variables));
