47de9301e5a1ab6300e9f4c8fe7a0abdae62e719
EmVal <147710433+EmanueleValentini1@users.noreply.github.com>
2024-10-10 12:51:22 +0200

    Dev (#126)
    Co-authored-by: GitHub Action <action@github.com>
    Co-authored-by: LuciaM1 <lucia.mazzocchi@nttdata.com>
    Co-authored-by: adrrossi <adriano.rossi@nttdata.com>
    Co-authored-by: adrrss <147711350+adrrss@users.noreply.github.com>

--- src/main/java/it/pagopa/wf/engine/service/CamundaService.java
+++ src/main/java/it/pagopa/wf/engine/service/CamundaService.java
@@ -17,11 +17,14 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.camunda.bpm.engine.impl.bpmn.parser.BpmnParse.CAMUNDA_BPMN_EXTENSIONS_NS;
 
 @Service
 public class CamundaService {
+
     @Autowired
     ProcessEngineConfigurationImpl processEngineConfiguration;
 
@@ -41,12 +44,16 @@ public class CamundaService {
             checkExecutable(bpmnParse.getRootElement());
             response.setIsVerified(Boolean.TRUE);
             response.setMessage("Correct Bpmn");
-            return response;
         } catch (Exception exception) {
+            String errorMessage = "Errore durante la validazione del BPMN. Controlla che il file sia conforme allo standard BPMN e ben formato.";
+            if (exception.getMessage() != null && !exception.getMessage().isEmpty()) {
+                errorMessage += " Dettagli: " + exception.getMessage();
+            }
+
             response.setIsVerified(Boolean.FALSE);
-            response.setMessage(exception.getCause() == null ? exception.getMessage() : exception.getCause().getMessage());
-            return response;
+            response.setMessage(errorMessage);
         }
+        return response;
     }
 
     public void checkExecutable(Element rootElement) {
@@ -60,11 +67,88 @@ public class CamundaService {
             } else {
                 throw new UnsupportedOperationException("non-executable process. Set the attribute isExecutable=true to deploy this process.");
             }
+
             String historyTimeToLiveString = processElement.attributeNS(CAMUNDA_BPMN_EXTENSIONS_NS, "historyTimeToLive");
-            if (historyTimeToLiveString == null || historyTimeToLiveString.isEmpty()){
+            if (historyTimeToLiveString == null || historyTimeToLiveString.isEmpty()) {
                 throw new UnsupportedOperationException("non-executable process. History Time To Live cannot be null.");
+            }
+
+            checkForJavaCode(processElement);
+        }
+    }
+
+    private void checkForJavaCode(Element processElement) {
+        List<Element> elementList = getAllElements(processElement);
+        for (Element bpmnElement : elementList) {
+
+            String javaClass = bpmnElement.attributeNS(CAMUNDA_BPMN_EXTENSIONS_NS, "class");
+
+            if (javaClass != null && !javaClass.isEmpty()) {
+                throw new UnsupportedOperationException("Rilevato codice Java: " + javaClass);
+            }
+
+            String scriptFormat = bpmnElement.attribute("scriptFormat");
+
+            if ("script".equals(bpmnElement.getTagName())) {
+                checkScriptTaskForJava(bpmnElement);
+            }
+
+            if ("groovy".equals(scriptFormat) || "java".equals(scriptFormat)) {
+                throw new UnsupportedOperationException("Rilevato script in linguaggio pericoloso (Java o Groovy) nel ScriptTask.");
             }
         }
     }
 
+    private List<Element> getAllElements(Element element) {
+        List<Element> elements = new ArrayList<>();
+        elements.add(element);
+
+        for (Element child : element.elements()) {
+            elements.addAll(getAllElements(child));
+        }
+
+        return elements;
+    }
+
+    void checkScriptTaskForJava(Element scriptTask) {
+        String scriptFormat = scriptTask.attribute("scriptFormat");
+
+        if ("javascript".equalsIgnoreCase(scriptFormat)) {
+
+            String scriptContent = scriptTask.getText();
+
+            if (containsJavaReferences(scriptContent)) {
+                throw new UnsupportedOperationException("Lo script JavaScript contiene riferimenti a codice Java non Consentito.");
+            }
+        } else if ("groovy".equalsIgnoreCase(scriptFormat) || "java".equalsIgnoreCase(scriptFormat)) {
+            throw new UnsupportedOperationException("Rilevato script in linguaggio pericoloso (Java o Groovy).");
+        }
+    }
+
+    boolean containsJavaReferences(String scriptContent) {
+
+        String[] javaPatterns = {
+                "task\\.",
+                "runtimeService\\.",
+                "importPackage\\(",
+                "Packages\\.",
+                "new java\\.",
+                "java\\.io\\." ,
+                "java\\.lang\\." ,
+                "java\\.net\\." ,
+                "java\\.nio\\." ,
+                "sun\\.misc\\.Unsafe" ,
+                "System\\." ,
+                "java\\.beans\\." ,
+                "java\\.security\\." ,
+                "javax\\.crypto\\." ,
+        };
+
+        for (String pattern : javaPatterns) {
+            if (scriptContent.matches("(?s).*" + pattern + ".*")) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
